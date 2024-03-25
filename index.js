6135b9cdac3c7efc2d08a193cc60049c0f571476
Justin <mca02097832@student.holmesglen.edu.au>
2024-03-26 10:36:18 +1100

    Update index.js
    removed duplicate resolvers and typedefs

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const { mergeTypeDefs, mergeResolvers } = require('@graphql-tools/merge');
 //connectoin
 const { MongoClient, ServerApiVersion } = require('mongodb');
 // const { ApolloServer } = require('apollo-server');
-const { typeDefsMerged, resolversMerged } = require('./graphql'); // Assuming your GraphQL schema and resolvers are in a separate file
+// const { typeDefsMerged, resolversMerged } = require('./graphql'); // Assuming your GraphQL schema and resolvers are in a separate file
 const debugStartup = require('./debugStartup');
 
 require('dotenv').config();
